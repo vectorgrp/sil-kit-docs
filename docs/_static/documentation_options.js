93402a7d69bdbddaf209c52854641cbd7944c978
Konrad Breitsprecher <Konrad.Breitsprecher@vector.com>
2023-07-10 15:21:40 +0200

    -- update docs to 4.0.31

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.0.30',
+    VERSION: '4.0.31',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
