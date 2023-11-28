13c6e84c245861b233b16b45dc93cd97d39b8b85
Daniel Edwards <Daniel.Edwards@vector.com>
2023-11-28 13:28:45 +0100

    -- update docs to 4.0.41

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.0.40',
+    VERSION: '4.0.41',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
