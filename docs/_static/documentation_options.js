fd899e85d449b7636b927aeb6b52b36d75684cc4
ci-user Build User <ci-user@vistrpndls013.vi.vector.int>
2025-05-20 12:51:29 +0200

    docs: update to version 4.0.56 build 3542623

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.0.55',
+    VERSION: '4.0.56',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
